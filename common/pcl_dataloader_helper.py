281e8919d9c19166bf35e3d1b816f4f22a4c6194
Frank Yu <54717271+yu-frank@users.noreply.github.com>
2020-11-12 13:08:13 -0800

    scale baseline

--- common/pcl_dataloader_helper.py
+++ common/pcl_dataloader_helper.py
@@ -49,6 +49,31 @@ def plot_the_skeletons(skel_2d):
     plt.ylim(1, -1)
     plt.show()
 
+def plot_the_large_skeletons(skel_2d):
+    # Plot 3 skeletons
+    middle = skel_2d.shape[0]//2
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    skel_0 = skel_2d[middle-10,:,:]
+    skel_1 = skel_2d[middle-5,:,:]
+    skel_2 = skel_2d[middle,:,:]
+    skel_3 = skel_2d[middle+5,:,:]
+    skel_4 = skel_2d[middle+10,:,:]
+    plot_list = [skel_0, skel_1, skel_2, skel_3, skel_4]
+    connections = [-1, 0, 1, 2, 0, 4, 5, 0, 7, 8, 9, 8, 11, 12, 8, 14, 15]
+    for n, plot_skel in enumerate(plot_list):
+        for i in range(1, plot_skel.shape[0]):
+            if n < 2:
+                color='blue'
+            if n == 2:
+                color='black'
+            if n > 2:
+                color='red'
+            ax.plot([plot_skel[i,0], plot_skel[connections[i],0]], [plot_skel[i,1], plot_skel[connections[i],1]], color=color)
+    plt.xlim(-2, 2)
+    plt.ylim(2, -2)
+    plt.show()
+
 def plot_the_single_skeletons_3d(skel_3d):
     # Plot 3 skeletons
     fig = plt.figure()
